c99b1f50456ee3f1408333a8496d215a91762da2
Luis Felipe Laiton Cortes <50175115+LuisLaiton@users.noreply.github.com>
2026-02-14 11:35:47 -0500

    reto 1 solucion

--- Semana-1/reto-1.js
+++ Semana-1/reto-1.js
@@ -1,1 +1,7 @@
-alert("Hola mundo")
+let num = 0, suma = 0;
+for (let i = 0; i < 10; i++) {
+    num = Number(prompt("Ingrese el valor del campo " + (i + 1)));
+    suma += num;
+}
+
+alert("La suma es: " + suma);
